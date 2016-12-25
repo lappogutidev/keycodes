4233d7e8e93c6c42b3bd337b8f20ffad9678fa7e
lappogutidev <lappoguti00@gmail.com>
2016-12-25 18:16:03 -0500

    added styles.css and set up the keycode listener

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.onload = function() {
+    var keycodeParagraph = document.getElementById("keycodeParagraph");
+    console.log("test : ", keycodeParagraph.innerHTML);
+}
+
 document.addEventListener("keydown", function(event) {
+  keycodeParagraph.innerHTML = event.which;
   console.log(event.which);
+  console.log(typeof event.which === "number");
 });
+
+var keycodeUpdate = function(keycode) {
+    keycodeParagraph.innerHTML = event.which;
+    
+}
