1507bf72e8dff9d06b9964c55bbfead50e0d5b0f
lappogutidev <lappoguti00@gmail.com>
2016-12-25 18:57:26 -0500

    created the keycode log

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 window.onload = function() {
     var keycodeParagraph = document.getElementById("keycodeParagraph");
     var keycodeDescription = document.getElementById("keycodeDescription");
+    var keycodeLog = document.getElementById("keycodeLog");
 }
 
 document.addEventListener("keydown", function(event) {
   keycodeUpdate(event);
+  console.log("Output = [", event.which, ":", event.key, "]");
 });
 
 var keycodeUpdate = function() {
     keycodeParagraph.innerHTML = event.which;
     keycodeDescription.innerHTML = event.key;
+    var logRow = keycodeLog.insertRow(keycodeLog.rows.length-1);
+    var logCell = logRow.insertCell(0);
+    logCell.innerHTML = "code : "+event.which+"key : "+event.key;
 }
