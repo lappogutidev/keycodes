6d02c5829191f1a434e034204a0c936ed5aa3668
lappogutidev <lappoguti00@gmail.com>
2016-12-25 19:06:51 -0500

    fixed spacebar absence

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ document.addEventListener("keydown", function(event) {
 var keycodeUpdate = function() {
     keycodeParagraph.innerHTML = event.which;
     keycodeDescription.innerHTML = event.key;
+    if (keycodeDescription.innerHTML === " ") {
+        keycodeDescription.innerHTML = "<br />";
+    };
     var logRow = keycodeLog.insertRow(keycodeLog.rows.length-1);
     var logCell = logRow.insertCell(0);
     logCell.innerHTML = event.which+" : "+event.key;
     if (keycodeLog.rows.length > 20) {
         keycodeLog.deleteRow(0);
-    }
+    };
 }
