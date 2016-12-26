6b733704da1b407ee2d09ac3728372760b5fd1c4
lappogutidev <lappoguti00@gmail.com>
2016-12-26 17:44:12 -0500

    reverse log direction

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ var keycodeUpdate = function() {
     if (keycodeDescription.innerHTML === " ") {
         keycodeDescription.innerHTML = "<br />";
     };
-    var logRow = keycodeLog.insertRow(keycodeLog.rows.length);
+    var logRow = keycodeLog.insertRow(0);
     var logCell1 = logRow.insertCell(0);
     var logCell2 = logRow.insertCell(1);
     logCell1.innerHTML = event.which;
     logCell2.innerHTML = event.key;
-    if (keycodeLog.rows.length > 20) {
-        keycodeLog.deleteRow(0);
-    };
+    console.log(event.which + ", " + event.key);
+    do {
+        keycodeLog.deleteRow(keycodeLog.rows.length - 1);
+    } while (keycodeLog.rows.length > 20);
 }
