7814efeb18ab3cb5086a88dcce3cbae3b7bdd514
lappogutidev <lappoguti00@gmail.com>
2016-12-27 14:17:43 -0500

    added directions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var keycodeParagraph;
+var directionUp = true;
+var maxLength = 20;
+var rowInsert = (function() {
+    if (directionUp){
+        return 0;
+    } else {
+        return maxLength - 1;
+    }
+})();
+var rowDelete = (maxLength - 1) - rowInsert;
 
 window.onload = function() {
     keycodeParagraph = document.getElementById("keycodeParagraph");
@@ -16,13 +26,18 @@ var keycodeUpdate = function() {
     if (keycodeDescription.innerHTML === " ") {
         keycodeDescription.innerHTML = "<br />";
     };
-    var logRow = keycodeLog.insertRow(0);
+    var logRow;
+    if (rowInsert === (maxLength -1)) {
+        logRow = keycodeLog.insertRow(this.keycodeLog.length);
+    } else {
+        logRow = keycodeLog.insertRow(rowInsert);
+    }
     var logCell1 = logRow.insertCell(0);
     var logCell2 = logRow.insertCell(1);
     logCell1.innerHTML = event.which;
     logCell2.innerHTML = event.key;
     console.log(event.which + ", " + event.key);
-    do {
-        keycodeLog.deleteRow(keycodeLog.rows.length - 1);
-    } while (keycodeLog.rows.length > 20);
+    while (keycodeLog.rows.length > maxLength) {
+        keycodeLog.deleteRow(rowDelete);
+    }
 }
